5b4f452cdda373a256497e0ddd33006b837084e4
Kyle Liang <lmanlai@techfin.ai>
2025-04-25 15:15:17 +0800

    init commit

--- Readme.md
+++ Readme.md
@@ -1,4 +1,105 @@
-Code will be coming soon.
+# LagKV
 
-[LagKV: Lag-Relative Information of the KV Cache Tells Which Tokens Are Important](https://arxiv.org/abs/2504.04704)
+#### Introduction
 
+LagKV is an efficient and robust KV compression algorithm. It uses lag tokens information to compress the previous ones which significantly boost the compression performance with little computation overhead.
+
+Details are in the following work:
+** [LagKV: Lag-Relative Information of the KV Cache Tells Which Tokens Are Important](https://arxiv.org/abs/2504.04704) **
+
+#### How to Use
+
+LagKV implements the Cache interface from transformers. It's easy to be integrated into the model calling function.
+
+```python
+from lag_kv import LagKV
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_path = "Qwen2.5-7B-Instruct"
+device = "cuda:0"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype="auto", attn_implementation="sdpa").to(device)
+
+prompt = "long text"
+input_ids = tokenizer.encode(prompt, return_tensors="pt").to(device)
+past_key_values = LagKV(lag_size=64)
+print(model.generate(input_ids, past_key_values=past_key_values))
+# check KV cache size
+print(past_key_values[0][0].size())
+```
+
+To compress the KV cache during the prefill stage instead of it's precisely calculated, you have to use the following inference function(for batch_size=1 only.):
+
+```python
+def inference_by_prefill_compress(model, tokenizer, inputs, max_new_tokens=256, decode=False, past_key_values=None, device="cuda"):
+    if isinstance(inputs, str):
+        input_ids = tokenizer([inputs], return_tensors="pt")["input_ids"].to(device)
+    else:
+        input_ids = inputs
+    if past_key_values is None:
+        past_key_values = LagKV(ratio=0.2,
+                             lag_size=128,
+                            layer_idx_skip_first=[],
+                             use_then_compress=True)
+    
+    with torch.no_grad():
+        sink_size = past_key_values.sink_size
+        lag_size = past_key_values.lag_size
+        trigger_len = sink_size + 2*lag_size
+        input_length = input_ids.shape[1]
+        # print(input_length > trigger_len)
+        if input_length > trigger_len:
+            start_idx = 0
+            end_idx = trigger_len
+            position_ids = torch.arange(input_length + max_new_tokens).unsqueeze(0).to(device)
+            def batch_input():
+                sel_input_ids = input_ids[:, start_idx:end_idx]
+                q_len = end_idx - start_idx
+                k_len = past_key_values.get_seq_length() + q_len
+                batch_size = input_ids.shape[0]
+                head_num = model.config.num_attention_heads
+                attn_mask = torch.zeros((batch_size, head_num, q_len, k_len), device=input_ids.device, dtype=torch.bool)
+                attn_mask[:] = False
+                attn_mask[:, :, -1] = True
+                for i in range(q_len-1):
+                    attn_mask[:, :, i, :-(q_len - i - 1)] = True
+                # If batch_size>1, the attention_mask will be more complicated because of the pad_tokens.
+                attention_mask = torch.zeros((batch_size, head_num, q_len, k_len), device=input_ids.device, dtype=torch.bfloat16)
+                attention_mask.masked_fill_(~attn_mask, -1000)
+                return {"input_ids": sel_input_ids, "attention_mask": attention_mask}
+            
+            while start_idx < input_length:
+                tmp_pos = position_ids[:, start_idx:end_idx]
+                outputs = model(**batch_input(), 
+                               past_key_values=past_key_values,
+                              position_ids=tmp_pos,
+                              cache_position=tmp_pos[0]
+                              )
+                start_idx = end_idx
+                end_idx += lag_size
+                end_idx = min(end_idx, input_length)
+
+            new_token_id = outputs.logits[:, -1].argmax(dim=-1).unsqueeze(-1)
+            # print(new_token_id)
+            new_token_count = 1
+            generated_ids = [new_token_id]
+            while new_token_id[0][0] != tokenizer.eos_token_id and new_token_count < max_new_tokens+1:
+                tmp_pos = position_ids[:, (input_length+new_token_count-1):(input_length+new_token_count)]
+                outputs = model(new_token_id, 
+                               past_key_values=past_key_values,
+                              position_ids=tmp_pos,
+                              cache_position=tmp_pos[0]
+                              )
+                new_token_id = outputs.logits[:, -1].argmax(dim=-1).unsqueeze(-1)
+                new_token_count += 1
+                generated_ids.append(new_token_id)
+            generated_ids = torch.cat(generated_ids, dim=-1)
+        else:
+            generated_ids = model.generate(inputs, do_sample=False, max_new_tokens=max_new_tokens, past_key_values=past_key_values)
+            generated_ids = generated_ids[:, input_length:]
+    if decode:
+        output = tokenizer.batch_decode(generated_ids)
+    else:
+        output = generated_ids
+    return output, past_key_values
+```
---
+++ l2_kv.py
@@ -0,0 +1,145 @@
+# Copyright 2025 China Merchants Bank. All rights reserved.
+#
+# Licensed under the MIT License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://mit-license.org
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from transformers.cache_utils import DynamicCache
+import torch
+
+
+class L2KV(DynamicCache):
+
+    def __init__(self, _distributed_cache_data = None,
+                 ratio: float = 0.5,
+                 sink_size: int = 16,
+                 lag_size: int = 1024,
+                 score_v_ratio: float = 0.0,
+                 use_low_norm: bool = True,
+                 skip_layer_idx: list = [0, 1],
+                 use_then_compress: bool = True, # important
+                ):
+        super().__init__(_distributed_cache_data)
+        self.ratio = ratio
+        self.sink_size = sink_size
+        self.lag_size = lag_size
+        self.score_v_ratio = score_v_ratio
+        self.use_low_norm = use_low_norm
+        self.skip_layer_idx = skip_layer_idx
+        self.use_then_compress = use_then_compress
+        self._compressed_len = []
+    
+    def update(
+        self,
+        key_states: torch.Tensor,
+        value_states: torch.Tensor,
+        layer_idx: int,
+        cache_kwargs = None,
+    ):
+        """
+        Updates the cache with the new `key_states` and `value_states` for the layer `layer_idx`.
+
+        Parameters:
+            key_states (`torch.Tensor`):
+                The new key states to cache.
+            value_states (`torch.Tensor`):
+                The new value states to cache.
+            layer_idx (`int`):
+                The index of the layer to cache the states for.
+            cache_kwargs (`Dict[str, Any]`, `optional`):
+                Additional arguments for the cache subclass. No additional arguments are used in `DynamicCache`.
+
+        Return:
+            A tuple containing the updated key and value states.
+        """
+        # Update the number of seen tokens
+        if layer_idx == 0:
+            self._seen_tokens += key_states.shape[-2]
+
+        # Update the cache
+        if key_states is not None:
+            if len(self.key_cache) <= layer_idx:
+                # There may be skipped layers, fill them with empty lists
+                for _ in range(len(self.key_cache), layer_idx):
+                    self.key_cache.append([])
+                    self.value_cache.append([])
+                    self._compressed_len.append(self.sink_size)
+                self.key_cache.append(key_states)
+                self.value_cache.append(value_states)
+                self._compressed_len.append(self.sink_size)
+            elif (
+                len(self.key_cache[layer_idx]) == 0
+            ):  # fills previously skipped layers; checking for tensor causes errors
+                self.key_cache[layer_idx] = key_states
+                self.value_cache[layer_idx] = value_states
+            else:
+                self.key_cache[layer_idx] = torch.cat([self.key_cache[layer_idx], key_states], dim=-2)
+                self.value_cache[layer_idx] = torch.cat([self.value_cache[layer_idx], value_states], dim=-2)
+                
+            if layer_idx not in self.skip_layer_idx:
+                return self._compress_kv_by_lag(layer_idx)
+        return self.key_cache[layer_idx], self.value_cache[layer_idx]
+
+    def _get_states_score(self, base_len, in_size, end_idx, value):
+        target_v = value[:, :, base_len:end_idx]
+        target_v = target_v.view(in_size[0], in_size[1], -1, self.lag_size, in_size[-1])
+        # ref = target_v[:, :, 1:, :, :]
+        v = target_v[:, :, :-1, :, :]
+
+        #min_r = ref.min(dim=-2).values.unsqueeze(-2).expand(-1, -1, -1, self.lag_size, -1)
+        #max_r = ref.max(dim=-2).values.unsqueeze(-2).expand(-1, -1, -1, self.lag_size, -1)
+
+        #score = ((v - min_r) / (max_r - min_r)).std(dim=-1).softmax(dim=-1)
+        score = v.norm(dim=-1)
+        if self.use_low_norm:
+            score *= -1
+        
+        return score        
+    
+    def _compress_algo(self, layer_idx, base_len):
+        in_size = self.key_cache[layer_idx].size()
+        end_idx = base_len + ((in_size[-2] - base_len) // self.lag_size) * self.lag_size
+        key_score = self._get_states_score(base_len, in_size, end_idx, self.key_cache[layer_idx])
+        value_score = self._get_states_score(base_len, in_size, end_idx, self.value_cache[layer_idx])
+        score = key_score + value_score * self.score_v_ratio
+        selected_idx = torch.topk(score, int(self.ratio * self.lag_size), dim=-1).indices
+        for i in range(1, selected_idx.size()[2]):
+            selected_idx[:, :, i] += i * self.lag_size
+        selected_idx = selected_idx.reshape(in_size[0], in_size[1], -1).unsqueeze(-1).expand(
+                                                            -1, -1, -1, in_size[-1])
+        new_base_len = base_len + selected_idx.size()[-2]
+        # alwarys keep the last window
+        tail_len = self.lag_size + in_size[-2] - end_idx
+        
+        def modify_kv(value):
+            selected_value = torch.gather(value[:, :, base_len:end_idx], -2, selected_idx)
+            value[:, :, base_len:new_base_len] = selected_value
+            # move value forward, clone for the case of overlap
+            value[:, :, new_base_len:(new_base_len + tail_len)] = value[:, :, -tail_len:].clone()
+            value = value[:, :, :(new_base_len + tail_len)]
+            return value
+            
+        self.key_cache[layer_idx] = modify_kv(self.key_cache[layer_idx])
+        self.value_cache[layer_idx] = modify_kv(self.value_cache[layer_idx])
+        self._compressed_len[layer_idx] = new_base_len
+    
+    def _compress_kv_by_lag(self, layer_idx):
+        kv_size = self.key_cache[layer_idx].size()
+        base_len = self._compressed_len[layer_idx]
+        tmp_key, tmp_value = self.key_cache[layer_idx], self.value_cache[layer_idx]
+        if kv_size[-2] >= base_len + 2*self.lag_size:
+            if self.use_then_compress:
+                tmp_key, tmp_value = self.key_cache[layer_idx].clone(), self.value_cache[layer_idx].clone()
+            self._compress_algo(layer_idx, base_len)
+            if not self.use_then_compress:
+                tmp_key, tmp_value = self.key_cache[layer_idx], self.value_cache[layer_idx]
+        return tmp_key, tmp_value
+        
